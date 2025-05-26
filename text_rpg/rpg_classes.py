4d05901e605a2c47e370853720722cb1aac8139c
남성현 <super157310@gmail.com>
2025-05-26 10:19:51 +0900

    a

--- text_rpg/rpg_classes.py
+++ text_rpg/rpg_classes.py
@@ -18,17 +18,17 @@ class Character:
 # 전사 클래스
 class Warrior(Character):
     def __init__(self, name):
-        super().__init__(name, health=150, attack=25, heal=5, mana =0) #각 수치 조정 필요
+        super().__init__(name, health=150, attack=50, heal=5, mana =0) #각 수치 조정 필요
 
 # 마법사 클래스 
 class Mage(Character):
     def __init__(self, name):
-        super().__init__(name, health=80, attack=35, heal=20, mana=30) #각 수치 조정 필요
+        super().__init__(name, health=80, attack=35, heal=20, mana=100) #각 수치 조정 필요
 
 # 성직자 클래스   
 class Priest(Character):
     def __init__(self, name):
-        super().__init__(name, health=80, attack=35, heal=20, mana=30) #각 수치 조정 필요
+        super().__init__(name, health=80, attack=25, heal=50, mana=30) #각 수치 조정 필요
 
 # 고블린1 클래스
 class Goblin1(Character):
@@ -43,4 +43,4 @@ class Mimic(Character):
 # 마왕 클래스 
 class Boss(Character):
     def __init__(self, name):
-        super().__init__(name, health=1000, attack=50, heal=0, mana=0) #각 수치 조정 필요
+        super().__init__(name, health=300, attack=30, heal=0, mana=0) #각 수치 조정 필요
